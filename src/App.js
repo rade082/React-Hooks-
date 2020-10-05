fd9ce7e5aac2969d3826bb44c0cc5d6d87ac1333
unknown <dutt2630@gmail.com>
2020-10-05 09:21:42 +0530

    reducer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import './App.css';
 import Greet from './components/Greet'
 import HookCall from './components/HookComp';
 import Fetching from './components/Fetching';
+import ComponentC from './components/ComponentC';
+import Reducer from './components/Reducer';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
 
 function App() {
   return (
     <div className="App">
       {/* <HookCall/> */}
-      <Fetching/>
+      {/* <Fetching/> */}
+      <Reducer/>
+      {/* <UserContext.Provider value={'rahul'}>
+        <ChannelContext.provider value={'dutt'}>
+              <ComponentC/>
+        </ChannelContext.provider>
+      </UserContext.Provider> */}
     </div>
   );
 }
